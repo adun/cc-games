63f46f07fd62d3137cd3c24584043dad0190a35b
adun <adun@users.noreply.github.com>
2023-11-26 00:57:39 +0100

    Create particle explosion on hit

--- index.js
+++ index.js
@@ -66,10 +66,42 @@ class Enemy {
   }
 }
 
+const friction = 0.99;
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
+  }
+
+  draw() {
+    c.save();
+    c.globalAlpha = this.alpha;
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+  }
+
+  update() {
+    this.draw();
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.alpha -= 0.01;
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const projectiles = [];
+const particles = [];
 const enemies = [];
 const player = new Player(x, y, 10, "white");
 
@@ -109,6 +141,14 @@ function animate() {
 
   player.draw();
 
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1);
+    } else {
+      particle.update();
+    }
+  });
+
   projectiles.forEach((projectile, index) => {
     projectile.update();
 
@@ -129,6 +169,8 @@ function animate() {
     enemy.update();
 
     const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+
+    // end game
     if (dist - enemy.radius - player.radius < 1) {
       cancelAnimationFrame(animationId);
     }
@@ -139,6 +181,22 @@ function animate() {
       // when projectile touch enemy
       if (dist - enemy.radius - projectile.radius < 1) {
         if (dist - enemy.radius - projectile.radius < 1) {
+          // create explosions
+          for (let i = 0; i < enemy.radius * 2; i++) {
+            particles.push(
+              new Particle(
+                projectile.x,
+                projectile.y,
+                Math.random() * 2,
+                enemy.color,
+                {
+                  x: (Math.random() - 0.5) * (Math.random() * 6),
+                  y: (Math.random() - 0.5) * (Math.random() * 6),
+                }
+              )
+            );
+          }
+
           if (enemy.radius - 10 > 5) {
             gsap.to(enemy, { radius: enemy.radius - 10 });
             setTimeout(() => {
